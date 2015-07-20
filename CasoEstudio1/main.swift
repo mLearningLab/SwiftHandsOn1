8a5eaf11d21f04049c7462fbb6e0bbffa109f723
Alberto Pacheco <apacheco@MacBook-Air-de-Alberto.local>
2015-07-20 13:50:28 -0600

    Initial Commit

---
+++ CasoEstudio1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CasoEstudio1
+//
+//  Created by Alberto Pacheco on 20/07/15.
+//  Copyright (c) 2015 Alberto Pacheco. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
