866e4b7583c22b918846e4ab1d38061089a08add
Alberto Pacheco <alberto@acm.org>
2015-07-21 02:18:11 -0600

    Juego Adivina
    Juego Adivina 1 al 1000, versión 1.0 (OS X Console App)

--- CasoEstudio1/main.swift
+++ CasoEstudio1/main.swift
@@ -1,44 +1,29 @@
 //
 //  main.swift
-//  CasoEstudio1
+//  Juego Adivina (Console Version ver. 1.0)
 //
 //  Created by Alberto Pacheco on 20/07/15.
 //  Copyright (c) 2015 Alberto Pacheco. All rights reserved.
 //
 
 import Foundation
-import Darwin // M_PI
 
-// Interpolación de Texto
-var radio = Double(inputNum("Radio"))
-let π = M_PI
-let area = Int(π * pow(radio,2))
+let oculto = randomNum(1...1000)
 
-println("El área del círculo es \(area)")
-println("El área del círculo es \(π * pow(radio,2))")
-
-//// CASO DE ESTUDIO \\\\
-var edad: Int?
-edad = inputNum("Edad")
-
-//// 1) Valor Opcional
-if edad != nil {
-    println("Caso 1: Tiene \(edad!) años")
+while true {
+    if let num = inputNum(”Anota número") {
+        if num == oculto {
+            println("GANASTE!")
+            exit(0)
+        } else if num < oculto {
+            println("Anota un número MAYOR")
+        } else {
+            println("Anota un número menor")
+        }
+    } else {
+        break
+    }
 }
 
-//// 2) let tmp
-if edad != nil {
-    let tmp = 12 * edad!
-    println("Caso 2: Tiene \(tmp) meses")
-}
-
-//// 3) if let
-if let tmp = edad {
-    println("Caso 3: Tiene \(12 * tmp) meses")
-}
-
-//// 4) implicit unwrap (!) && nil coalescing op (??)
-var n: Int! = inputNum("Anote edad") ?? 0
-println("Caso 4: Tiene \(12 * n) meses")
 
 println("\nBye!")
