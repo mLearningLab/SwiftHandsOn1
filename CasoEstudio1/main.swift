8fda09e8e2d35c081549b1d68639e6cc7a510489
albertochiwas <alberto@acm.org>
2015-07-21 03:18:06 -0600

    Some exercises

--- CasoEstudio1/main.swift
+++ CasoEstudio1/main.swift
@@ -5,6 +5,10 @@
 //  Created by Alberto Pacheco on 20/07/15.
 //  Copyright (c) 2015 Alberto Pacheco. All rights reserved.
 //
+// Ejercicio:
+// 1) Modifica ciclo para ofrecer sólo 12 intentos (si se agotan termina e imprime valor oculto)
+// 2) Sustituye los "if num" por una sentencia switch/cases, usando patrones
+// 3) Si la entrada no es un número, dar oportunidad de repetir intento (no reducir intentos)
 
 import Foundation
 
