93360dd82680d674cf140585d38d753f565ead16
Alberto Pacheco <alberto@acm.org>
2015-07-21 03:06:32 -0600

    Update WorkshopLibrary.swift
    Commentaries

--- CasoEstudio1/WorkshopLibrary.swift
+++ CasoEstudio1/WorkshopLibrary.swift
@@ -1,6 +1,6 @@
 //
 //  WorkshopLibrary.swift
-//  CasoEstudio1
+//  Library functions
 //
 //  Created by Alberto Pacheco on 20/07/15.
 //  Copyright (c) 2015 Alberto Pacheco. All rights reserved.
@@ -8,11 +8,13 @@
 
 import Foundation
 
+// Returns an integer random number inside a closed interval (default=1...1000)
 func randomNum(_ range: ClosedInterval<Int> = 1...1000) -> Int
 {
     return Int(arc4random_uniform(UInt32(range.end + 1))) + range.start
 }
 
+// Read a string from keyboard (receives and echoes a text label)
 func input(t: String) -> String
 {
     print("\(t)? ")
@@ -20,6 +22,8 @@ func input(t: String) -> String
     return NSString(data: keyboard.availableData, encoding:NSUTF8StringEncoding)!.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
 }
 
+// Read an integer number within a closed interval from keyboard (default range = 1...1000)
+// Note: Returns nil if input string is not a number
 func inputNum(t: String, range: ClosedInterval<Int> = 1...1000) -> Int!
 {
     if let n = input(t).toInt() {
