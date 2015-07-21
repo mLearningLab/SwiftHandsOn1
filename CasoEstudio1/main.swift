1c0ebe2def48665d6f0b1b9b4ae8b3eac405bea5
Alberto Pacheco <alberto@acm.org>
2015-07-21 02:40:47 -0600

    Update main.swift
    Commentaries

--- CasoEstudio1/main.swift
+++ CasoEstudio1/main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  CasoEstudio1
+//  Library functions (usage demo)
 //
 //  Created by Alberto Pacheco on 20/07/15.
 //  Copyright (c) 2015 Alberto Pacheco. All rights reserved.
@@ -8,10 +8,13 @@
 
 import Foundation
 
+// randomNum( anIntRange ) -> Int
 println("randomNum-->[\(randomNum(1...1000))]")
 
+// input( _:String ) -> String
 var s = input("Name")
 println("input-->[\(s)]")
 
+// inputNum( _:String, anIntRange ) -> Int?
 var t = inputNum("Age")
 println("inputNum-->[\(t)]")
